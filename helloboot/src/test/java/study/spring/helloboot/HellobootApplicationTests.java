8ff1454efa1b13fb612b2b630d5295d48a19a673
YoungChulShin <go1323@gmail.com>
2023-01-22 20:58:52 +0900

    hello spring

---
+++ helloboot/src/test/java/study/spring/helloboot/HellobootApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HellobootApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
