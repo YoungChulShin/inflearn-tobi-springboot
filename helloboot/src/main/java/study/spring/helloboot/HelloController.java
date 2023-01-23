37470fcfb048617e7ba2d1c0510b821313550e96
YoungChulShin <go1323@gmail.com>
2023-01-23 23:04:04 +0900

    servlet을 dispatcher servlet으로 변경

--- helloboot/src/main/java/study/spring/helloboot/HelloController.java
+++ helloboot/src/main/java/study/spring/helloboot/HelloController.java
@@ -1,7 +1,12 @@
 package study.spring.helloboot;
 
 import java.util.Objects;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+// 1차적으로 class level을 참고하고, 그 다음에 method level을 참고한다
+@RequestMapping
 public class HelloController {
 
   private final HelloService helloService;
@@ -10,7 +15,10 @@ public class HelloController {
     this.helloService = helloService;
   }
 
+  @GetMapping("/hello")
+  @ResponseBody
   public String hello(String name) {
+    System.out.println("hello");
     return helloService.sayHello(Objects.requireNonNull(name));
   }
 }
--- helloboot/src/main/java/study/spring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/study/spring/helloboot/HellobootApplication.java
@@ -1,33 +1,27 @@
 package study.spring.helloboot;
 
-import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.web.context.support.GenericWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class HellobootApplication {
 
   public static void main(String[] args) {
+    // Spring Container 생성
     GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
     applicationContext.registerBean(HelloController.class);
     applicationContext.registerBean(SimpleHelloService.class);
     applicationContext.refresh(); // create bean object
 
+    // Servlet Webserver 생성 및 실행
     ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
     WebServer webServer = serverFactory.getWebServer(servletContext -> {
       servletContext.addServlet(
-          "dispatcherServlet",
-          new DispatcherServlet(applicationContext));
+              "dispatcherServlet",
+              new DispatcherServlet(applicationContext))
+          .addMapping("/*");
     });
 //    WebServer webServer = serverFactory.getWebServer(servletContext -> {
 //      servletContext.addServlet("frontcontroller", new HttpServlet() {
