9ad10d333143f68549808fe1dd816842c7dbddca
YoungChulShin <go1323@gmail.com>
2023-01-24 00:04:24 +0900

    리팩토링
    - ServletWebServerFactory bean 등록
    - DispatcherServlet bean 등록
    - MySpringApplication 으로 기능 분리

--- helloboot/src/main/java/study/spring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/study/spring/helloboot/HellobootApplication.java
@@ -1,5 +1,6 @@
 package study.spring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -14,25 +15,17 @@ import org.springframework.web.servlet.DispatcherServlet;
 @ComponentScan
 public class HellobootApplication {
 
-  public static void main(String[] args) {
-    // Spring Container 생성
-    AnnotationConfigWebApplicationContext applicationContext = new AnnotationConfigWebApplicationContext() {
-      @Override
-      protected void onRefresh() {
-        super.onRefresh();
+  @Bean
+  public ServletWebServerFactory servletWebServerFactory() {
+    return new TomcatServletWebServerFactory();
+  }
 
-        // Servlet Webserver 생성 및 실행
-        ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
-        WebServer webServer = serverFactory.getWebServer(servletContext -> {
-          servletContext.addServlet(
-                  "dispatcherServlet",
-                  new DispatcherServlet(this))
-              .addMapping("/*");
-        });
-        webServer.start();
-      }
-    };
-    applicationContext.register(HellobootApplication.class);
-    applicationContext.refresh(); // create bean object
+  @Bean
+  public DispatcherServlet dispatcherServlet() {
+    return new DispatcherServlet();
+  }
+
+  public static void main(String[] args) {
+    MySpringApplication.run(HellobootApplication.class, args);
   }
 }
