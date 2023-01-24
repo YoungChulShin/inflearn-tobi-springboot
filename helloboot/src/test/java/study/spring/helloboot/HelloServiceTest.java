a3a72fa55acf087a5f6d26a2eaf3a1182d3ab140
YoungChulShin <go1323@gmail.com>
2023-01-24 23:59:23 +0900

    meta annotation

--- helloboot/src/test/java/study/spring/helloboot/HelloServiceTest.java
+++ helloboot/src/test/java/study/spring/helloboot/HelloServiceTest.java
@@ -1,7 +1,25 @@
 package study.spring.helloboot;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target(ElementType.METHOD)
+//@UnitTest
+//@interface FastUnitTest {
+//
+//}
+//
+//@Retention(RetentionPolicy.RUNTIME)
+//@Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+//@Test
+//@interface UnitTest {
+//
+//}
 
 public class HelloServiceTest {
 
