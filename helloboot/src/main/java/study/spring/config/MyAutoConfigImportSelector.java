61402a3b19771cc7d909484ef565c276c00a2b81
YoungChulShin <go1323@gmail.com>
2023-01-29 01:12:42 +0900

    설정 import를 file을 이용해서 가져오도록 수정

--- helloboot/src/main/java/study/spring/config/MyAutoConfigImportSelector.java
+++ helloboot/src/main/java/study/spring/config/MyAutoConfigImportSelector.java
@@ -1,15 +1,24 @@
 package study.spring.config;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
 
+  private final ClassLoader classLoader;
+
+  public MyAutoConfigImportSelector(ClassLoader classLoader) {
+    this.classLoader = classLoader;
+  }
+
   @Override
   public String[] selectImports(AnnotationMetadata importingClassMetadata) {
-    return new String[] {
-        "study.spring.config.autoconfig.DispatcherServletConfig",
-        "study.spring.config.autoconfig.TomcatWebServerConfig"
-    };
+    List<String> autoConfigs = new ArrayList<>();
+    ImportCandidates.load(MyAutoConfiguration.class, classLoader).forEach(autoConfigs::add);
+
+    return autoConfigs.toArray(String[]::new);
   }
 }
