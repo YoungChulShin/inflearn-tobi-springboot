f0c8da30ae97a26e6a16483dc65a292ae5da649f
YoungChulShin <go1323@gmail.com>
2023-01-29 00:34:47 +0900

    ImportSelector를 이용해서 config 클래스를 코드에서 관리하도록 수정

--- helloboot/src/main/java/study/spring/config/EnableMyAutoConfiguration.java
+++ helloboot/src/main/java/study/spring/config/EnableMyAutoConfiguration.java
@@ -4,13 +4,11 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.context.annotation.Import;
-import study.spring.config.autoconfig.DispatcherServletConfig;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@Import({DispatcherServletConfig.class, TomcatServletWebServerFactory.class})
+@Import(MyAutoConfigImportSelector.class)
 public @interface EnableMyAutoConfiguration {
 
 }
