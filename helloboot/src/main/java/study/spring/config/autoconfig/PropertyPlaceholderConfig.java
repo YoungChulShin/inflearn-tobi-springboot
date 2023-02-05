10bf2e37bba309868c0a52e0753bfb9307607551
YoungChulShin <go1323@gmail.com>
2023-02-05 22:19:04 +0900

    value를 사용해서 속성 읽어오기

---
+++ helloboot/src/main/java/study/spring/config/autoconfig/PropertyPlaceholderConfig.java
@@ -0,0 +1,15 @@
+package study.spring.config.autoconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import study.spring.config.MyAutoConfiguration;
+
+@MyAutoConfiguration
+public class PropertyPlaceholderConfig {
+
+  @Bean
+  PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+    return new PropertySourcesPlaceholderConfigurer();
+  }
+
+}
